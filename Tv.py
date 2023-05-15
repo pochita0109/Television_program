b4a0b46216aecaed0eb3439e66f2dbbf051023a3
pochita0109 <akosicosta@gmail.com>
2023-05-15 09:59:05 -0700

    Pseudocodes

---
+++ Tv.py
@@ -0,0 +1,16 @@
+# Kenneth John Costa
+# Assignment 6
+# TV Program
+
+# Create a TV class
+# Set attributes for TV class
+# Turn on the TV
+# Turn off the TV
+# Get the channel of TV
+# Set new channel of TV
+# Get the volume of TV
+# Set new volume of TV
+# Increase the channel by 1
+# Decrease the channel by 1
+# Increase the volume by 1
+# Decrease the volume by 1
