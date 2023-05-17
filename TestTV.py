7b80a04b80a752b5542a97ee152dfc1a25907661
pochita0109 <akosicosta@gmail.com>
2023-05-16 20:41:09 -0700

    Added number of channel and volume in TV1

--- TestTV.py
+++ TestTV.py
@@ -2,6 +2,12 @@
 from Tv import TV
 def test_drive():
 # TV1
-# Print the output
+    TV1 = TV()
+    TV1.turn_on()
+    TV1.set_channel(30)
+    TV1.set_channel(3)
+    # Print the output
+    print(f"The TV1's channel is {TV1.get_channel()}, and volume level is {TV1.get_volume_level()}")
 # TV2
 # Print the output
+test_drive()
