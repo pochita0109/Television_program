e9dfd7542d50b212d79d01d95b16a44c0a377c2b
pochita0109 <akosicosta@gmail.com>
2023-05-16 22:46:24 -0700

    Changed TV1 and TV2 because the output for channel and volume is always 1

--- TestTV.py
+++ TestTV.py
@@ -2,17 +2,13 @@
 from Tv import TV
 def test_drive():
 # TV1
-    TV1 = TV()
+    TV1 = TV(channel = 30, volume_level = 3)
     TV1.turn_on()
-    TV1.set_channel(30)
-    TV1.set_channel(3)
     # Print the output
     print(f"The TV1's channel is {TV1.get_channel()}, and volume level is {TV1.get_volume_level()}")
 # TV2
-    TV2 = TV()
+    TV2 = TV(channel = 3, volume_level = 2)
     TV2.turn_on()
-    TV2.set_channel(3)
-    TV2.set_channel(2)
     # Print the output
     print(f"The TV2's channel is {TV2.get_channel()}, and volume level is {TV2.get_volume_level()}")
 test_drive()
